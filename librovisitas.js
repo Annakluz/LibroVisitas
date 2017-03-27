acdc1cfa06398bc9c694dc6f393b7eb85ae5ba46
Annakluz <anna.luz.duran@gmail.com>
2017-03-27 00:03:02 -0600

    tratando de ver que pasa con js

--- librovisitas.js
+++ librovisitas.js
@@ -1,1 +1,14 @@
 
+
+var texto = document.getElementById("texto");
+var textoGenerado = document.getElementById("textoResultado");
+
+/*
+ Lo primero que necesito hacer depues de haber sacado las variables  es una funcion que me permita mostrar el texto generado*/
+function generarTexto() {
+    textoGenerado.innerHTML = texto.value;
+}
+
+function grande(){
+    textoGenerado.style.fontSize = "30px";
+}
